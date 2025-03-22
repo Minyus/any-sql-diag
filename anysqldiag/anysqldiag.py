a8ed3826f4519318c5d3573cb760477169464181
Minyus <Minyus@github.com>
2025-03-22 20:51:27 +0800

    release v0.0.3

--- anysqldiag/anysqldiag.py
+++ anysqldiag/anysqldiag.py
@@ -1,17 +1,35 @@
 import json
+from pathlib import Path
+import sys
 import typing as t
 
 import sqlglot
 
 
 def anysqldiag(
-    sql: str,
+    sql: str = None,
+    file: str = None,
+    from_stdin: bool = False,
+    dialect: t.Optional[str] = None,
     error_level: t.Optional[str] = None,
     error_message_context: int = 100,
     max_errors: int = 3,
-    dialect: str = None,
     json_indent: int = 2,
 ):
+
+    if from_stdin:
+        assert not sql, sql
+        assert not file, file
+        sql = "\n".join(sys.stdin)
+    elif file:
+        assert not sql, sql
+        assert not from_stdin, from_stdin
+        sql = Path(file).read_text()
+    else:
+        assert not file, file
+        assert not from_stdin, from_stdin
+        assert sql, sql
+
     error_level = (
         getattr(sqlglot.ErrorLevel, error_level.upper()) if error_level else None
     )
